0233468b9fccd73e217d49a03c8c7b58d1f29969
ardevtech7 <ardevtech7@gmail.com>
2023-01-21 15:08:22 +0900

    bubble sort 구현

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void main() {
+        // Given
+        String[] args = {"2", "1", "3"};
+
+        // When & Then
+        Main.main(args);
+    }
+}
